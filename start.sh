c78c90e741f504d0911c97482f9d7e94f48c4a4c
srinivasnayak001 <sn673052@gmail.com>
2026-02-05 19:17:45 +0530

    Add deployment script2

--- start.sh
+++ start.sh
@@ -7,21 +7,19 @@ echo "Creating default superuser..."
 
 python manage.py shell << END
 from django.contrib.auth import get_user_model
-import os
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+username = "Srinivas"
+email = "srinivas@gmail.com"
+password = "Sri@1234"
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        print("Superuser created.")
-    else:
-        print("Superuser already exists.")
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
 END
 
 echo "Starting Gunicorn..."
-gunicorn lostfound.wsgi
+gunicorn lostfound.wsgi:application
